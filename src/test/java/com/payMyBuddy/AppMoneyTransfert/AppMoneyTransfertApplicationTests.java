bdf6ffdca01cc2eab1f27d371bcfa83d3a7918ff
Denis Siveton <denis.siveton@hotmail.com>
2021-10-03 11:42:07 +0200

    database configuration set

---
+++ src/test/java/com/payMyBuddy/AppMoneyTransfert/AppMoneyTransfertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payMyBuddy.AppMoneyTransfert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMoneyTransfertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
